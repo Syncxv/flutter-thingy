23a93d397aed57d24b235129a0af39ea68c72da6
Syncxv <aruldevarackal1@gmail.com>
2022-01-25 16:47:50 +0530

    sheesh lookin good

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,4 +1,5 @@
 import 'package:appy/models/models.dart';
+import 'package:appy/widgets/todo_bottom_tab_bar.dart';
 import 'package:flutter/material.dart';
 
 class TodoScreen extends StatefulWidget {
@@ -13,39 +14,41 @@ class _TodoScreenState extends State<TodoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        extendBody: true,
+        bottomNavigationBar: TodoBttomTabBar(),
         body: CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          actions: [
-            Container(
-              margin: const EdgeInsets.all(6.0),
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                shape: BoxShape.circle,
+          slivers: [
+            SliverAppBar(
+              actions: [
+                Container(
+                  margin: const EdgeInsets.all(6.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.close),
+                    iconSize: 30.0,
+                    color: Colors.black,
+                  ),
+                )
+              ],
+              backgroundColor: Colors.white,
+              floating: true,
+              centerTitle: false,
+              title: Text(
+                widget.todo.name,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(Icons.close),
-                iconSize: 30.0,
-                color: Colors.black,
-              ),
-            )
-          ],
-          backgroundColor: Colors.white,
-          floating: true,
-          centerTitle: false,
-          title: Text(
-            widget.todo.name,
-            style: const TextStyle(
-              color: Colors.black,
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
             ),
-          ),
-        ),
-      ],
-    ));
+          ],
+        ));
   }
 }
---
+++ lib/widgets/todo_bottom_tab_bar.dart
@@ -0,0 +1,139 @@
+import 'package:appy/models/models.dart';
+import 'package:appy/widgets/button.dart';
+import 'package:flutter/material.dart';
+
+class TodoBttomTabBar extends StatefulWidget {
+  const TodoBttomTabBar({Key? key}) : super(key: key);
+
+  @override
+  State<TodoBttomTabBar> createState() => _TodoBttomTabBarState();
+}
+
+class _TodoBttomTabBarState extends State<TodoBttomTabBar> {
+  bool completed = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        boxShadow: const [],
+        border: Border.all(
+          width: 1,
+          color: Colors.transparent,
+        ), //color is transparent so that it does not blend with the actual color specified
+        gradient: const LinearGradient(
+          end: Alignment.topCenter,
+          begin: Alignment.bottomCenter,
+          stops: [0.25, 0.90],
+          colors: [
+            Colors.white,
+            Color.fromRGBO(255, 255, 255, 0.0),
+          ],
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Button(
+            icon: Icons.add,
+            text: "New Task",
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (_) => TaskDaialog(),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TaskDaialog extends StatefulWidget {
+  TaskDaialog({Key? key}) : super(key: key);
+
+  @override
+  State<TaskDaialog> createState() => Task_DaialogState();
+}
+
+class Task_DaialogState extends State<TaskDaialog> {
+  @override
+  bool completed = false;
+  TextEditingController taskNameController = TextEditingController();
+  TextEditingController taskDescriptionController = TextEditingController();
+  void submitTask() {
+    Navigator.of(context).pop('dialog');
+  }
+
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      insetPadding: const EdgeInsets.fromLTRB(0, 45, 0, 0),
+      child: Container(
+        color: Colors.white,
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Colors.white.withOpacity(0.9),
+              centerTitle: true,
+              floating: true,
+              flexibleSpace: TextField(
+                controller: taskNameController,
+                onEditingComplete: submitTask,
+                textAlignVertical: TextAlignVertical.center,
+                autofocus: true,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(10.0),
+                  hintStyle: const TextStyle(fontSize: 14.0),
+                  hintText: "Name Of Task",
+                  alignLabelWithHint: true,
+                  prefixIcon: IconButton(
+                    onPressed: () => setState(() => completed = !completed),
+                    icon: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.blueAccent,
+                        ),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Opacity(
+                        opacity: completed ? 1 : 0,
+                        child: const Icon(
+                          Icons.check_circle,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                  suffixIcon: IconButton(
+                    padding: const EdgeInsets.all(10.0),
+                    onPressed: submitTask,
+                    icon: const Icon(Icons.check),
+                  ),
+                ),
+              ),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              sliver: SliverToBoxAdapter(
+                child: TextField(
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    hintStyle: TextStyle(fontSize: 14.0),
+                    hintText: "Description (optinal)",
+                    alignLabelWithHint: true,
+                  ),
+                  controller: taskDescriptionController,
+                  keyboardType: TextInputType.multiline,
+                  minLines: 24,
+                  maxLines: null,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
